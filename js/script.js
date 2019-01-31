2ff3ed3e3aec8b8dd8165cb73e3b7e74c54490eb
Nikstep <Nikstepwn@gmail.com>
2019-01-31 12:05:24 +0100

    v0.12 - All pages created and added basic layout

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
 $(document).ready(function(){
-$('.slider').slick({
+
+/*
+* SLIDER via Slick
+*/
+  $('.slider').slick({
   centerMode: true,
   autoplay: true,
   autoplaySpeed: 3000,
   centerPadding: '25%',
   slidesToShow: 1,
+  speed: 800,
   responsive: [
     {
       breakpoint: 1500,
       settings: {
         centerMode: true,
         centerPadding: '20%',
-        slidesToShow: 1
+        slidesToShow: 1,
+        speed: 800,
       }
     },
     {
