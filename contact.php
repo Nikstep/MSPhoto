210dc9d8972d925fcd1f7d5979cce89d236dbb36
Nikstep <Nikstepwn@gmail.com>
2019-02-12 10:05:51 +0100

    added responsivity

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+
+if (isset($_POST["submit"]))
+    $name = $_POST["name"];
+    $phone = $_POST["phone"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $mailTO = "nikstepwn@gmail.com";
+    $headers = "Form from: MSPhoto";
+    $txt = "Přišla ti zpráva od ".$name ".\n\n".$message "telefon:".$phone "email: ".$email;
+
+    mail($mailTO, $txt, $headers);
+
+    header("Location: index.html?mailsend");
+
+?>
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready( function(){
 
 /*
 * SLIDER via Slick
@@ -21,7 +21,7 @@ $(document).ready(function(){
       }
     },
     {
-      breakpoint: 768,
+      breakpoint: 760,
       settings: {
         arrows: false,
         centerMode: true,
@@ -45,33 +45,37 @@ $(document).ready(function(){
 * IMAGE HOVER
 */
 
-
+//Eventualy made via CSS.
 
 /*
 * LIGHTBOX
 */
 
+//Created hidden overlay and attached to end of body.
 var overlay = $("<div/>", { id: "overlay" });
     overlay.appendTo("body").hide(),
+//Created hidden image without url and attached to overlay.
     image = $("<img/>", { id: "image"}),
     image.appendTo("#overlay").hide();
-    
+//On click at href in gallery, extracted and stored in var href its href attribute.
+//@"a" transfered after handler for AJAX implemented gallery at /omně.html.
 $(".gallery").on("click", "a", function() {
   event.preventDefault();
    
    var href = $(this).attr("href");
+//Image in overlay gets its href attribute.
    $("#image").attr("src", href);
-
+//Show overlay.
     overlay.html( image.show() )
             .fadeIn(400);
    
 });
-
+//close option1
 $(document).on("click", "#overlay", function() {
     $(this).fadeOut(400);
 });
 
-
+//close option2
 $(document).on("keyup", function() {
     if( event.which == 27) {
         overlay.fadeOut(400);
@@ -86,21 +90,53 @@ $(document).on("keyup", function() {
 
     var gallery = $(".gallery"),
         selected = $(".controls").find(".selected");
-
+//On click at link in controls, extracted its href attribute.
     $(".controls a").on("click", function(event) {
         event.preventDefault();
 
         var a = $(this),
             li = a.parent(),
             href = a.attr("href");
-
+//Preventing load of preselected gallery.
         if (selected.is( li ) ) return;
 
         selected = li;
-
+//Change of selected link appearance.
         li.addClass("selected").siblings().removeClass("selected");
-
+//Load gallery-set from selected page.
         gallery.load(href + " .gallery-set");
     });
+
+/**
+ * RESPONSIVITY
+ */
+
+//Toggle of hamburger menu and animation of regular.
+$(".hamburger").on("click", function () {
+    $(this).toggleClass("isActive");
+    if ($(".menu").hasClass("isOn")) {
+      $(".menu").slideUp(500).removeClass("isOn");
+      $(".social").fadeOut(300).removeClass("isOn");
+    }
+    else {$(".menu").slideDown(500).addClass("isOn");
+    $(".social").fadeIn(300).addClass("isOn");}
+    // $(".menu").toggleClass("isOn");
+    // $(".social").toggleClass("isOn");
+});
+
+//As position of menu is fixed, it will be hidden with scroll. 
+
+var match = window.matchMedia("(max-width: 1024px)" );
+if (match.matches) {
+  $(window).scroll( function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 0) {
+      $(".social").removeClass("isOn");
+      $(".hamburger").removeClass("isActive");
+      $(".menu").slideUp(500).removeClass("isOn");
+    }
+  });
+
+}
 
 });
