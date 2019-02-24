03f2a8095e1b240839498db0d62815c0b3e74963
Nikstep <Nikstepwn@gmail.com>
2019-02-24 11:45:57 +0100

    Form update

--- contact.php
+++ contact.php
@@ -1,17 +1,19 @@
 <?php
 
-if (isset($_POST["submit"]))
+    if (isset($_POST['submit'])) {
+
     $name = $_POST["name"];
     $phone = $_POST["phone"];
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    $mailTO = "nikstepwn@gmail.com";
-    $headers = "Form from: MSPhoto";
-    $txt = "Přišla ti zpráva od ".$name ".\n\n".$message "telefon:".$phone "email: ".$email;
+    $to = "nikstepwn@gmail.com";
+    $subject = "Form from: MSPhoto";
+    $headers = "Mail from:". $email;
+    $txt = 'Přišla ti zpráva od: '.$name.".\n\n". $message;
 
-    mail($mailTO, $txt, $headers);
+    mail($to, $subject, $txt, $headers);
 
-    header("Location: index.html?mailsend");
+    }
 
 ?>
